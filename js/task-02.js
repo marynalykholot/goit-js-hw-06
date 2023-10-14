f744b905216c075ec095e2ffc3daaf4e66fd2f3a
Maryna Lykholot <marina.litkovets96@gmail.com>
2023-10-14 17:14:59 +0300

    таск2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientEl = document.querySelector('#ingredients');
+const resalt = ingredients.map ((ingredient) => {
+  const item = document.createElement('li') ;
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+
+ingredientEl.append(...resalt);
