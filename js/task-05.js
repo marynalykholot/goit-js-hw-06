99d1c7884828f612c1c546cfdffe5bd7c653838c
Maryna Lykholot <marina.litkovets96@gmail.com>
2023-10-17 15:16:03 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+// Напиши скрипт, який під час набору тексту в інпуті
+//  input#name-input (подія input), підставляє його поточне значення
+//   в span#name-output. Якщо інпут порожній,
+//  у спані повинен відображатися рядок "Anonymous".
+
+const inputRow = document.querySelector('#name-input');
+const inputRow2 = document.querySelector('#name-output');
+
+inputRow.addEventListener('input',inputOn);
+function inputOn(evt){
+   inputRow2.textContent = evt.currentTarget.value;
+   if (evt.currentTarget.value === '') {
+    inputRow2.textContent = "Anonymous";
+   }
+};
